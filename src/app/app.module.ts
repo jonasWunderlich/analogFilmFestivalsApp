457fbbb72c9f9274e67fc14384e07c0f17a49b70
Jonas Wunderlich <jonas@systemkultur.de>
2023-03-28 16:59:25 +0200

    update root store module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,15 @@
 import { InjectionToken, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import {
-  CommonModule,
   HashLocationStrategy,
   LocationStrategy,
   registerLocaleData,
 } from '@angular/common';
 import localeDe from '@angular/common/locales/de';
 import { NgxTmdbApiModule } from '@igorissen/ngx-tmdb-api';
-import { LetModule } from '@ngrx/component';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AuditoriumListModule } from './features/auditorium-list/auditorium-list.module';
-import { CalendarModule } from './features/calendar/calendar.module';
-import { CinemaListModule } from './features/cinema-list/cinema-list.module';
-import { CinemaMapModule } from './features/cinema-map/cinema-map.module';
-import { MovieListModule } from './features/movie-list/movie-list.module';
-import { ProjectionListModule } from './features/projection-list/projection-list.module';
-import { ReportListModule } from './features/report-list/report-list.module';
-import { TwoColumnLayoutModule } from './features/two-column-layout/two-column-layout.module';
-import { CinemaStoreModule } from './root-store/cinema-store/cinema-store.module';
-import { MovieStoreModule } from './root-store/movie-store/movie-store.module';
-import { ReportStoreModule } from './root-store/report-store/report.module';
-import { ScreeningEventStoreModule } from './root-store/screening-event-store/screening-event-store.module';
+import { RootStoreModule } from './root-store/root-store.module';
 registerLocaleData(localeDe);
 
 export const APP_TITLE = new InjectionToken<string>('app-title');
@@ -38,30 +19,7 @@ export const APP_TITLE = new InjectionToken<string>('app-title');
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LetModule,
-    // Store Modules
-    StoreModule.forRoot({}, {}),
-    EffectsModule.forRoot([]),
-    StoreRouterConnectingModule.forRoot(),
-    StoreDevtoolsModule.instrument({
-      maxAge: 25, // Retains last 25 states
-      logOnly: environment.production, // Restrict extension to log-only mode
-    }),
-    CommonModule,
-    ReportStoreModule,
-    ScreeningEventStoreModule,
-    MovieStoreModule,
-    CinemaStoreModule,
-    //StoreRootModule,
-    //Feature Modules
-    MovieListModule,
-    CinemaListModule,
-    ReportListModule,
-    ProjectionListModule,
-    AuditoriumListModule,
-    CinemaMapModule,
-    CalendarModule,
-    TwoColumnLayoutModule,
+    RootStoreModule,
     NgxTmdbApiModule.forRoot({ apiKey: '05180a707de5ada5dc9a38cd1f8da87b' }),
   ],
   providers: [
--- src/app/root-store/root-store.module.ts
+++ src/app/root-store/root-store.module.ts
@@ -1,9 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from 'src/environments/environment';
+import { CinemaStoreModule } from './cinema-store/cinema-store.module';
+import { MovieStoreModule } from './movie-store/movie-store.module';
+import { ReportStoreModule } from './report-store/report.module';
+import { ScreeningEventStoreModule } from './screening-event-store/screening-event-store.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    // TODO
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
+    CommonModule,
+    ReportStoreModule,
+    ScreeningEventStoreModule,
+    MovieStoreModule,
+    CinemaStoreModule,
   ],
 })
 export class RootStoreModule {}
