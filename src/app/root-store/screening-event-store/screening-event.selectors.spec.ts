a68beda5433aa920ab3377e751935df6bd6b3cc7
Jonas Wunderlich <jonas@systemkultur.de>
2023-04-01 18:02:20 +0200

    add minor unit test

--- src/app/root-store/screening-event-store/screening-event.selectors.spec.ts
+++ src/app/root-store/screening-event-store/screening-event.selectors.spec.ts
@@ -1,5 +1,19 @@
+import { loadScreeningEvents } from './screening-event.actions';
 import * as fromEvent from './screening-event.reducer';
 import { selectScreeningEventState } from './screening-event.selectors';
+
+const initState = {
+  activeScreeningEventId: undefined,
+  ids: [],
+  entities: {},
+  loadingStates: {
+    loadingScreeningEvent: false,
+    loadingScreeningEvents: false,
+    createScreeningEvent: false,
+    deleteScreeningEvent: false,
+    updateScreeningEvent: false,
+  },
+};
 
 describe('Event Selectors', () => {
   it('should select the feature state', () => {
@@ -7,6 +21,12 @@ describe('Event Selectors', () => {
       [fromEvent.screeningEventFeatureKey]: {},
     });
 
-    expect(result).toEqual({});
+    expect(result).toEqual(initState);
+  });
+
+  it('should enable loading flag for loadScreeningEvents', () => {
+    const action = loadScreeningEvents();
+    const newState = fromEvent.reducer(initState, action);
+    expect(newState.loadingStates.loadingScreeningEvents).toBe(true);
   });
 });
