dfc95379654178cc76f422e0aebdb0d29c887e74
Jonas Wunderlich <jonas@systemkultur.de>
2023-04-11 15:28:18 +0200

    update models 2

--- src/app/core/utilities/store-selectors.utilities.ts
+++ src/app/core/utilities/store-selectors.utilities.ts
@@ -1,14 +1,28 @@
 import { Store } from '@ngrx/store';
 import { updateCinemasOnMap } from 'src/app/+state/cinema-store/cinema.actions';
 
-export function selectReferencedObject<T extends { id: string }>(
+interface EntityWithId {
+  id: string;
+}
+
+export interface EntityWithCinemaRefs {
+  cinemaRefs: string[];
+  id: string;
+}
+
+export interface EntityWithEventRefs {
+  eventRefs: string[];
+  id: string;
+}
+
+export function selectReferencedObject<T extends EntityWithId>(
   arr: Array<T>,
   reference: string
 ): T | undefined {
   return arr.find((item) => item.id === reference);
 }
 
-export function selectReferencedObjects<T extends { id: string }>(
+export function selectReferencedObjects<T extends EntityWithId>(
   arr: Array<T>,
   references: string[]
 ): Array<T> {
@@ -22,9 +36,17 @@ export function dispatchMap(ids: string[] | undefined, store: Store): void {
 }
 
 export function dispatchUpdateCinemapMapFromEntities<
-  T extends { cinemaRefs: string; id: string }
+  T extends EntityWithCinemaRefs
 >(arr: Array<T>, store: Store): void {
   const cinemaRefs = arr.map((item) => item.cinemaRefs).flat();
   const withoutDuplicates = [...new Set(cinemaRefs)];
   store.dispatch(updateCinemasOnMap({ ids: withoutDuplicates }));
+}
+
+export function filterEntitiesByIds<T extends EntityWithId>(
+  ids: string[],
+  entities: T[] = []
+): T[] {
+  console.log(entities, ids);
+  return entities.filter((item) => ids.indexOf(item.id) > -1);
 }
